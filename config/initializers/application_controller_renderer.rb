341cfef25546adf632f784ff3f65756ea4fbaf83
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-01-25 05:36:37 +0000

    Update files

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e482397bc7871b6fa1228fecba3f92be2c7eca4da40d718bd8342d444ad047670b4fd36c3dca3265e97161366ddb3bbd26dc34375bcbc34387696d4273969928'
